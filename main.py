b2366db7976aaaa9741a3c467063cd9530462946
eric-castillo05 <ericcastillo005@gmail.com>
2023-08-15 13:32:32 -0600

    refactor: reorganize lines of code

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import dash
 from dash import html
 from dash import dcc
 import pandas as pd
+app = dash.Dash(__name__)
 app.layout = html.Div(
     children=[
         dcc.Graph(figure=None)
     ]
 )
-app = dash.Dash(__name__)
 if __name__ == '__main__':
     app.run_server(degub=True)
